7c682fc333233200dd07ac16918af1b3dd5d2cd3
736659711@qq.com <736659711@qq.com>
2018-12-17 06:42:10 +0800

    i

--- js/upRight.js
+++ js/upRight.js
@@ -16,26 +16,34 @@ function upRight_getAllData() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			let str = xmlhttp.responseText
 			let data = JSON.parse(str)
-			//console.log(data)
+			let time = new Array()
+			var dataAll = new Array()
+			dataAll.push(data.data)
+			upRight_getAllData2()
 
-			let dataSetType = {
-				"ALLSUM": "",
-				"EIGHT": "",
-				"ELEVEN": "",
-				"FIVE": "",
-				"FOUR": "",
-				"NAME": "",
-				"NINE": "",
-				"ONE": "",
-				"SEVEN": "",
-				"SIX": "",
-				"TEN": "",
-				"THREE": "",
-				"TWELVE": "",
-				"TWO": ""
+
+
+			function upRight_getAllData2() {
+				let xmlhttp;
+				if (window.XMLHttpRequest) {
+					xmlhttp = new XMLHttpRequest();
+				} else {
+					xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				//回调函数
+				xmlhttp.onreadystatechange = function() {
+					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+						let str = xmlhttp.responseText
+						let data = JSON.parse(str)
+						dataAll.push(data.data)
+						upRight_addDiv('UpRightGraph', dataAll)
+					}
+				}
+				console.log("downRight_allData")
+				xmlhttp.open("POST", "http://www.pipicat.top:5000/rest/allcom", true);
+				xmlhttp.setRequestHeader("Content-type", "application/json");
+				xmlhttp.send(JSON.stringify({}));
 			}
-			upRight_addDiv('UpRightGraph', data.data)
-
 		}
 	}
 	console.log("downRight_allData")
@@ -43,6 +51,7 @@ function upRight_getAllData() {
 	xmlhttp.setRequestHeader("Content-type", "application/json");
 	xmlhttp.send(JSON.stringify({}));
 }
+
 
 
 //初始化一个节点，用来存放控件
@@ -99,12 +108,13 @@ function upRight_addDiv(id, data) {
 function upRight_layout(code, id, data) {
 	console.log(data)
 	var myCharts = echarts.init(document.getElementById(id));
+	myCharts.clear()
 	if (code == 0) {
-		upRight_lineMonthAmount(data)
+		upRight_lineMonthAmount(data[0])
 	} else if (code == 1) {
-		upRight_countOfTypes(data)
+		upRight_countOfTypes(data[0])
 	} else if (code == 2) {
-
+		upRight_Circle(data[1])
 	} else if (code == 3) {
 
 	}
@@ -232,6 +242,126 @@ function upRight_layout(code, id, data) {
 
 		myCharts.setOption(option);
 
+
+	}
+
+	function upRight_Circle(data) {
+		var weekDay = ["星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+		var myDate
+		var dic_date = {}
+		dic_date['星期一'] = 0
+		dic_date['星期二'] = 0
+		dic_date['星期三'] = 0
+		dic_date['星期四'] = 0
+		dic_date['星期五'] = 0
+		dic_date['星期六'] = 0
+		dic_date['星期天'] = 0
+		for (let i in data) {
+			let time_str = ''
+			time_str = '2015-' + data[i]
+			myDate = new Date(Date.parse(time_str))
+			dic_date[weekDay[myDate.getDay()]] += 1
+		}
+		var dic_arr = []
+		var dic = {}
+		dic['name'] = '星期一'
+		dic['value'] = dic_date['星期一']
+		dic_arr.push(dic)
+		var dic2 = {}
+		dic2['name'] = '星期二'
+		dic2['value'] = dic_date['星期二']
+		dic_arr.push(dic2)
+		var dic3 = {}
+		dic3['name'] = '星期三'
+		dic3['value'] = dic_date['星期三']
+		dic_arr.push(dic3)
+		var dic4 = {}
+		dic4['name'] = '星期四'
+		dic4['value'] = dic_date['星期四']
+		dic_arr.push(dic4)
+		var dic5 = {}
+		dic5['name'] = '星期五'
+		dic5['value'] = dic_date['星期五']
+		dic_arr.push(dic5)
+		var dic6 = {}
+		dic6['name'] = '星期六'
+		dic6['value'] = dic_date['星期六']
+		dic_arr.push(dic6)
+		var dic7 = {}
+		dic7['name'] = '星期天'
+		dic7['value'] = dic_date['星期天']
+		dic_arr.push(dic7)
+		
+
+		let option = {
+			tooltip: {
+				trigger: 'item',
+				formatter: "{a} <br/>{b} : {c} ({d}%)"
+			},
+			legend: {
+				orient: 'vertical',
+				x: 'left',
+				data: ['星期一', '星期二', '星期三', '星期四', '星期五','星期六','星期天']
+			},
+			toolbox: {
+				show: true,
+				feature: {
+					mark: {
+						show: true
+					},
+					dataView: {
+						show: true,
+						readOnly: false
+					},
+					magicType: {
+						show: true,
+						type: ['pie', 'funnel'],
+						option: {
+							funnel: {
+								x: '25%',
+								width: '50%',
+								funnelAlign: 'center',
+								max: 1548
+							}
+						}
+					},
+					restore: {
+						show: true
+					},
+					saveAsImage: {
+						show: true
+					}
+				}
+			},
+			calculable: true,
+			series: [{
+				name: '分布',
+				type: 'pie',
+				radius: ['50%', '70%'],
+				itemStyle: {
+					normal: {
+						label: {
+							show: false
+						},
+						labelLine: {
+							show: false
+						}
+					},
+					emphasis: {
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								fontSize: '20',
+								fontWeight: 'bold'
+							}
+						}
+					}
+				},
+				data: dic_arr
+			}]
+		};
+		myCharts.setOption(option);
 
 	}
 
